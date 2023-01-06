e8c1102bd1f811b0a4633dbc47135c6cc11909a8
Yuhui <acs102145@gmail.com>
2023-01-07 00:53:52 +0800

    first commit

---
+++ Yu_Blazor_Template/Extensions/ConfigureExtension.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph.ExternalConnectors;
+using Microsoft.JSInterop;
+using System.Globalization;
+using System.Reflection.Metadata;
+
+namespace Yu_Blazor_Template.Extensions
+{
+    public static class ConfigureExtension
+    {
+        public static async Task LoadSettingJson(this WebAssemblyHostConfiguration configuration, HttpClient httpClient)
+        {
+            using var response = await httpClient.GetAsync("yusettings.json");
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            configuration.AddJsonStream(stream);
+        }
+
+        public static void SetMsAuth(this WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                options.ProviderOptions.Authentication.ClientId = builder.Configuration["MsAuth:ClientID"];
+                //options.ProviderOptions.Authentication.Authority =
+                //    string.Format("https://login.microsoftonline.com/{0}", Constant.MS_AUTH_TENANT_ID);
+                //Microsoft identity platform v2.0才支援多租用戶，但目前不支援&prompt=select_account選擇帳號
+                //Azure AD 資訊清單 - allowPublicClient:true, signInAudience: AzureADandPersonalMicrosoftAccount
+                options.ProviderOptions.Authentication.Authority = "https://login.microsoftonline.com/common";
+                options.ProviderOptions.Authentication.ValidateAuthority = true;
+                options.ProviderOptions.LoginMode = "redirect";
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Files.Read");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Files.Read.All");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Files.ReadWrite");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Files.ReadWrite.All");
+            });
+        }
+
+        public static async Task SetCulture(this WebAssemblyHost host)
+        {
+            CultureInfo culture;
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+            culture = new CultureInfo("zh-Hant");
+            await js.InvokeVoidAsync("blazorCulture.set", "zh-Hant");
+            /*if (result != null)
+            {
+                culture = new CultureInfo(result);
+            }
+            else
+            {
+                culture = new CultureInfo("zh-Hant");
+                await js.InvokeVoidAsync("blazorCulture.set", "zh-Hant");
+            }*/
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+    }
+}
---
+++ Yu_Blazor_Template/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using System.IO;
+using Yu_Blazor_Template;
+using Yu_Blazor_Template.Extensions;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+HttpClient httpClient = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+builder.Services.AddScoped(sp => httpClient);
+await builder.Configuration.LoadSettingJson(httpClient);
+
+builder.Services.AddMudServices();
+builder.Services.AddGraphClient();
+builder.SetMsAuth();
+
+//»y¨t
+builder.Services.AddLocalization(option =>
+{
+    option.ResourcesPath = "Resources";
+});
+
+var host = builder.Build();
+//»y¨t
+await host.SetCulture();
+await host.RunAsync();
